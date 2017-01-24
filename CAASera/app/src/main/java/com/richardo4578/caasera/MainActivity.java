c44dd796883ea8b391b8fe058fa1a95fee9a11b4
ravensdispatch <ravensdispatch@gmail.com>
2017-01-24 15:20:45 -0800

    Added buttons and banner.

--- CAASera/app/src/main/java/com/richardo4578/caasera/MainActivity.java
+++ CAASera/app/src/main/java/com/richardo4578/caasera/MainActivity.java
@@ -2,12 +2,35 @@ package com.richardo4578.caasera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtLoginName, edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtLoginName = (EditText) findViewById(R.id.edtLoginName);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
     }
+
+    public void btnLoginOnClick(View v) {
+        Log.i("CLICK", "Login button was clicked.");
+        Log.i("CLICK", edtLoginName.getText().toString());
+        Log.i("CLICK", edtPassword.getText().toString());
+        Toast.makeText(this, "You have successfully logged in as: " + edtLoginName.getText().toString() + "\nPassword: " + edtPassword.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void btnCancelOnClick(View v) {
+        Log.i("CLICK", "Cancel button was clicked.");
+        Toast.makeText(this, "Login cancelled!", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
